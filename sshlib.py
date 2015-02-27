bfed694a89ac978daacd723cba55f9577eee4d12
Celso Braz <celso.braz@gmail.com>
2015-02-27 01:13:28 +0000

    some cleanup

--- sshlib.py
+++ sshlib.py
@@ -2,27 +2,20 @@ import subprocess
 import sys
 
 
-def cleanup_output(result):
+def cleanup_output(content):
   clean = []
-  for line in result:
+  for line in content:
     clean.append(line.decode().rstrip('\n'))
 
   return clean 
 
 def execute(host, command):
-#HOST="192.168.1.127"
-# Ports are handled in ~/.ssh/config since we use OpenSSH
-#COMMAND="df -h"
 
   ssh = subprocess.Popen(["ssh", "%s" % host, command],
     shell=False,
     stdout=subprocess.PIPE,
     stderr=subprocess.PIPE)
-  result = ssh.stdout.readlines()
-  error = ssh.stderr.readlines()
-  #print("ERROR:",error)
-  return {"stdout" : cleanup_output(result),"stderr" : cleanup_output(error)}
-#    result = cleanup_output(result)
-#    for line in result:
-#      print(line)
+  stdout = ssh.stdout.readlines()
+  stderr = ssh.stderr.readlines()
+  return {"stdout" : cleanup_output(stdout),"stderr" : cleanup_output(stderr)}
 
