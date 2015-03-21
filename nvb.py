92fcef24d952605662f5636c788db2683bb9555f
cbraz <celso.braz@gmail.com>
2015-03-21 01:51:29 +0000

    bla

---
+++ nvb.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+from libs import runlocal
+from time import sleep
+import subprocess
+import os
+
+
+sleep_stop=3600
+sleep_kill=5
+while True:
+
+  os.environ['DISPLAY'] = ":1"
+  print("Starting chromium")
+  subprocess.Popen(["chromium", "https://gateway.playneverwinter.com", "&"],env=os.environ, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+
+  print("Executing for", sleep_stop, "seconds")
+  sleep(sleep_stop)
+  result = runlocal.execute("ps -fe")
+  for line in result["stdout"]:
+    if "chromium" in line:
+      my_list = line.split()
+      print("found chromium process:", my_list[1], ", killing it")
+      runlocal.execute("kill "+my_list[1])
+
+  sleep(sleep_kill)
+  result = runlocal.execute("ps -fe")
+  for line in result["stdout"]:
+    if "chromium" in line:
+      my_list = line.split()
+      print("found chromium process:", my_list[1], ", killing it")
+      runlocal.execute("kill -9 "+my_list[1])
+
+
